71997f30301ad9d4517aa738be6d5e6f322cb6c1
jin28931343-stack <jin28931343@gmail.com>
2025-12-23 03:15:13 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mci-sim-v1.3.2-offline-fix'; // 更新版本號以觸發更新
+const CACHE_NAME = 'mci-sim-v1.3.3-offline-fix'; // 更新版本號以觸發更新
 
 // 補齊了 HTML 中所有引用的資源 (圖片與音效)
 const ASSETS_TO_CACHE = [
@@ -147,6 +147,7 @@ self.addEventListener('activate', (event) => {
         })
     );
 });
+
 
 
 
